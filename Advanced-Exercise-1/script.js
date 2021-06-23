778712d8b555e77d4286915b3ce8bcf7f5103c46
Nikos Batzonis <n.batzonis@gmail.com>
2021-06-23 14:06:11 +0200

    Finish Advanced Exercise 1

---
+++ Advanced-Exercise-1/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var a = 7;
+var b = 14;
+var c = "21";
+var d = '36';
+var e = 42;
+
+var sum = a + b + c + d + e;
+console.log(sum);
+
+var f = '1';
+var g = 15;
+var h = 8;
+var i = "1";
+
+var sum1 = f * g * h * i;
+console.log(sum1);
+
+var divide = sum / sum1;
+console.log(divide);
--- Advanced-Exercise-1/strict.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var a = 7;
-var b = 14;
-var c = "21";
-var d = '36';
-var e = 42;
-
-var sum =
