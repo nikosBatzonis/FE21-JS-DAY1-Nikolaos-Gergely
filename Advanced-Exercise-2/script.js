a31e4288f0da3caa65e1507f0214d30d8d64af57
Gergely Izsak <izsak.gera@gmail.com>
2021-06-23 14:21:25 +0200

    finished

---
+++ Advanced-Exercise-2/script.js
@@ -0,0 +1,15 @@
+var arr = [
+  [1, 2, 1, 24],
+  [8, 11, 9, 4],
+  [7, 0, 7, 27],
+  [7, 4, 28, 14],
+  [3, 10, 25, 7],
+  [21, 4, 6, 17],
+  [3, 5, 26, 3],
+];
+
+console.log(arr[1][1]);
+console.log(arr[4][2]);
+console.log(arr[5][3]);
+console.log(arr[2][3]);
+console.log(arr[2][1]);
