53fa928f8d33419d0381178d33dfed12c8479bb8
Gergely Izsak <izsak.gera@gmail.com>
2021-06-23 15:33:06 +0200

    not ready yet

--- Challenge-1/script.js
+++ Challenge-1/script.js
@@ -25,9 +25,10 @@ let arr1 = [
 console.log(arr1);
 
 console.log(arr1[0][0]);
-console.log(arr1[1][0]);
+console.log(arr1[1][1]);
 console.log(arr1[2][2]);
 console.log(arr1[3][3]);
 console.log(arr1[4][4]);
 
-document.write(arr1[0][0]), (arr1[1][1]);
+document.write(arr1[0][0] + " " + arr1[1][1] + " " + arr1[2][2] + " " + arr1[3][3] + " " + arr1[4][4]);
+
